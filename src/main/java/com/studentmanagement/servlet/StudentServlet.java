38438c3383096415eacf76b1e1d5cd06f82f5e90
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-02-11 14:00:55 +0200

    adding the servlet and db util

--- src/main/java/com/studentmanagement/servlet/StudentServlet.java
+++ src/main/java/com/studentmanagement/servlet/StudentServlet.java
@@ -1,4 +1,43 @@
 package com.studentmanagement.servlet;
 
-public class StudentServlet {
+import com.studentmanagement.beans.Student;
+import com.studentmanagement.dao.StudentDao;
+import com.studentmanagement.util.DBUtil;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class StudentServlet  extends HttpServlet {
+    protected void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException , IOException {
+        try(Connection connection  = DBUtil.getConnection()){
+            StudentDao dao=new StudentDao(connection);
+            List<Student> students = dao.getAllStudents();
+            request.setAttribute("students",students);
+            RequestDispatcher dispatcher= request.getRequestDispatcher("/ListStudents.jsp");
+            dispatcher.forward(request,response);
+        }catch(SQLException e){
+            throw new ServletException("Database error",e);
+        }
+    }
+    protected void doPost(HttpServletRequest request ,HttpServletResponse response){
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        Student student = new Student();
+        student.setName(name);
+        student.setEmail(email);
+        try(Connection connection =DBUtil.getConnection()){
+            StudentDao dao =  new StudentDao(connection);
+            dao.addStudent(student);
+            response.sendRedirect("list");
+        }catch (SQLException e){
+            throw new ServletException("Database error",e);
+        }
+    }
 }
--- src/main/java/com/studentmanagement/util/DBUtil.java
+++ src/main/java/com/studentmanagement/util/DBUtil.java
@@ -1,4 +1,19 @@
 package com.studentmanagement.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBUtil {
+    private static final String URL= "jdbc:postgresql://localhost:5432/jsp_student";
+    private static final String username = "postgres";
+    private static final String password = "postgres";
+    public static Connection getConnection() throws SQLException{
+        try {
+            return DriverManager.getConnection(URL, username, password);
+        }catch( SQLException e){
+            throw  new SQLException("Database connection error",e);
+        }
+    }
+
 }
